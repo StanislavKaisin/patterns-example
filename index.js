6e1bcc2ff0e440b547714a484d8d2755a107e11b
StanislavKaisin <8050361@gmail.com>
2020-10-23 21:18:36 +0300

    constructor

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 //1. Creational
 //1) constructor
+
+/* old school
+
+function Server(name, ip) {
+  this.name = name;
+  this.ip = ip;
+}
+
+Server.prototype.getURL = function () {
+  return `https://${this.ip}:80`;
+};
+
+*/
+
+class Server {
+  constructor(name, ip) {
+    this.name = name;
+    this.ip = ip;
+  }
+  getURL = function () {
+    return `https://${this.ip}:80`;
+  };
+}
+
+const aws = new Server("AWS German", "82.21.21.32");
+
+console.log("aws.getURL()=", aws.getURL());
