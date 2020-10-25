f9844aceb614b21202453b7e06358d0659791b2c
StanislavKaisin <8050361@gmail.com>
2020-10-25 19:40:02 +0200

    adapter

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ members.forEach((member) => {
 });
 */
 
-//Prototype
+//3) Prototype
 /*
 car = {
   wheels: 4,
@@ -98,7 +98,8 @@ carWithOwner.init();
 console.log("carWithOwner.__proto__ === car=", carWithOwner.__proto__ === car);
 */
 
-//singleton
+//4) singleton
+/*
 class Database {
   constructor(data) {
     if (Database.exists) {
@@ -118,3 +119,60 @@ console.log("mongo.getData()=", mongo.getData());
 
 const mysql = new Database("MySQL");
 console.log("mysql.getData()=", mysql.getData());
+*/
+
+//2. Structural
+//5) adapter
+
+class OldCalc {
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case "add":
+        return t1 + t2;
+      case "sub":
+        return t1 - t2;
+      default:
+        return NaN;
+    }
+  }
+}
+
+class NewCalc {
+  add(t1, t2) {
+    return t1 + t2;
+  }
+  sub(t1, t2) {
+    return t1 - t2;
+  }
+}
+
+class CalcAdapter {
+  constructor() {
+    this.calc = new NewCalc();
+  }
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case "add":
+        return this.calc.add(t1, t2);
+      case "sub":
+        return this.calc.sub(t1, t2);
+      default:
+        return NaN;
+    }
+  }
+}
+
+const oldCalc = new OldCalc();
+console.log(
+  'oldCalc.operations(10, 5, "add")=',
+  oldCalc.operations(10, 5, "add")
+);
+
+const newCalc = new NewCalc();
+console.log("newCalc.add(10, 5)=", newCalc.add(10, 5));
+
+const adapter = new CalcAdapter();
+console.log(
+  'adapter.operations(25, 10, "sub")=',
+  adapter.operations(25, 10, "sub")
+);
