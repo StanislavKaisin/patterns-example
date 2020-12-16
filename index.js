876acd725629b628dffbd124ee65b5bc12603d61
StanislavKaisin <8050361@gmail.com>
2020-12-16 20:57:17 +0200

    decorator

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ console.log("mysql.getData()=", mysql.getData());
 //2. Structural
 //5) adapter
 
+/*
 class OldCalc {
   operations(t1, t2, operation) {
     switch (operation) {
@@ -176,3 +177,39 @@ console.log(
   'adapter.operations(25, 10, "sub")=',
   adapter.operations(25, 10, "sub")
 );
+*/
+
+//6) decarator
+
+class Server {
+  constructor(ip, port) {
+    this.ip = ip;
+    this.port = port;
+  }
+
+  get url() {
+    return `https://${this.ip}:${this.port}`;
+  }
+}
+
+function aws(server) {
+  server.isAWS = true;
+  server.awsInfo = function () {
+    return server.url;
+  };
+  return server;
+}
+
+function azure(server) {
+  server.isAzure = true;
+  server.port += 500;
+  return server;
+}
+
+const s1 = aws(new Server("12.34.56.78", 8080));
+console.log("s1.isAWS=", s1.isAWS);
+console.log("s1.awsInfo()=", s1.awsInfo());
+
+const s2 = azure(new Server("98.87.76.12", 1000));
+console.log("s2.isAzure=", s2.isAzure);
+console.log("s2.url=", s2.url);
