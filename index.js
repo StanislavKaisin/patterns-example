907fdd300f2a2a66c2533e52f2a5066baf9d4f38
StanislavKaisin <8050361@gmail.com>
2020-12-18 22:07:47 +0200

    Command

--- index.js
+++ index.js
@@ -323,6 +323,7 @@ console.log(proxiedFetch("angular.io"));
 
 // iii behaviour
 //10 chain of responsibility
+/*
 class MySum {
   constructor(initialValue = 42) {
     this.sum = initialValue;
@@ -338,7 +339,38 @@ class MySum {
 const sum1 = new MySum();
 console.log("sum1=", sum1);
 console.log(sum1.add(8));
+// the chain itself
 console.log(sum1.add(8).add(10).add(1).add(9).sum);
 
 const sum2 = new MySum(0);
 console.log(sum2.add(1).add(2).add(3).add(4).sum);
+*/
+
+// 11 command
+class MyMath {
+  constructor(initialValue = 0) {
+    this.num = initialValue;
+  }
+  square() {
+    return this.num ** 2;
+  }
+  cube() {
+    return this.num ** 3;
+  }
+}
+
+class Command {
+  constructor(subject) {
+    this.subject = subject;
+    this.commandsExecuted = [];
+  }
+  execute(command) {
+    this.commandsExecuted.push(command);
+    return this.subject[command]();
+  }
+}
+const x = new Command(new MyMath(2));
+console.log("x=", x);
+console.log("x.execute('square')=", x.execute("square"));
+console.log("x.execute('cube')=", x.execute("cube"));
+console.log("x.commandsExecuted=", x.commandsExecuted);
