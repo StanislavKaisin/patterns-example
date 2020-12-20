3d4349b5a271a5b2815d8234038edb729569574e
StanislavKaisin <8050361@gmail.com>
2020-12-20 21:20:41 +0200

    observer

--- index.js
+++ index.js
@@ -428,6 +428,7 @@ console.log("gen.next().value=", gen.next().value);
 */
 
 //13 mediator
+/*
 class User {
   constructor(name) {
     this.name = name;
@@ -475,3 +476,62 @@ vlad.send("Hello!!", lena);
 lena.send("Hello!! hello", vlad);
 igor.send("hello world");
 igor.send("hello Igor", igor);
+*/
+
+// 14 observer
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+  unsubscriber(observer) {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+  fire(action) {
+    this.observers.forEach((observer) => {
+      observer.update(action);
+    });
+  }
+}
+class Observer {
+  constructor(state = 1) {
+    this.state = state;
+    this.initialState = state;
+  }
+  update(action) {
+    switch (action.type) {
+      case "INCREMENT":
+        this.state = ++this.state;
+        break;
+      case "DECREMENT":
+        this.state = --this.state;
+        break;
+      case "ADD":
+        this.state = this.state + action.payload;
+        break;
+      default:
+        this.state = this.initialState;
+    }
+  }
+}
+
+const stream$ = new Subject();
+const obs1 = new Observer();
+const obs2 = new Observer(42);
+
+stream$.subscribe(obs1);
+stream$.subscribe(obs2);
+
+console.log("obs1.state=", obs1.state);
+console.log("obs2.state=", obs2.state);
+
+stream$.fire({ type: "INCREMENT" });
+stream$.fire({ type: "INCREMENT" });
+stream$.fire({ type: "INCREMENT" });
+stream$.fire({ type: "DECREMENT" });
+stream$.fire({ type: "ADD", payload: 10 });
+
+console.log("obs1.state=", obs1.state);
+console.log("obs2.state=", obs2.state);
