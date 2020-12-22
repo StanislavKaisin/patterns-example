71744832d401e127395a194d83b4af91d919f182
StanislavKaisin <8050361@gmail.com>
2020-12-22 22:41:51 +0200

    state

--- index.js
+++ index.js
@@ -479,6 +479,7 @@ igor.send("hello Igor", igor);
 */
 
 // 14 observer
+/*
 class Subject {
   constructor() {
     this.observers = [];
@@ -535,3 +536,64 @@ stream$.fire({ type: "ADD", payload: 10 });
 
 console.log("obs1.state=", obs1.state);
 console.log("obs2.state=", obs2.state);
+*/
+
+//15 state
+class Light {
+  constructor(light) {
+    this.light = light;
+  }
+}
+
+class RedLight extends Light {
+  constructor() {
+    super("red");
+  }
+  sign() {
+    return "STOP";
+  }
+}
+class YellowLight extends Light {
+  constructor() {
+    super("yellow");
+  }
+  sign() {
+    return "PREPARE";
+  }
+}
+class GreenLight extends Light {
+  constructor() {
+    super("green");
+  }
+  sign() {
+    return "RUN";
+  }
+}
+
+class TrafficLight {
+  constructor() {
+    this.states = [new RedLight(), new YellowLight(), new GreenLight()];
+    this.current = this.states[0];
+  }
+  change() {
+    const total = this.states.length;
+    let index = this.states.findIndex((light) => light === this.current);
+    if (index + 1 < total) {
+      this.current = this.states[index + 1];
+    } else {
+      this.current = this.states[0];
+    }
+  }
+  sign() {
+    return this.current.sign();
+  }
+}
+
+const traffic = new TrafficLight();
+console.log("traffic.sign(1)=", traffic.sign());
+traffic.change();
+console.log("traffic.sign(2)=", traffic.sign());
+traffic.change();
+console.log("traffic.sign(3)=", traffic.sign());
+traffic.change();
+console.log("traffic.sign(4)=", traffic.sign());
